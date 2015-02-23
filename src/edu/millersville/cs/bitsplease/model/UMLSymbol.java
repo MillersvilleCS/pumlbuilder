7db35c9d7bd79360182642159752dd22196d47ad
Kevin Fisher <kevincliffordfisher@gmail.com>
2015-02-23 11:23:15 -0500

    Added EventDispatcher implementation

--- src/edu/millersville/cs/bitsplease/model/UMLSymbol.java
+++ src/edu/millersville/cs/bitsplease/model/UMLSymbol.java
@@ -1,5 +1,14 @@
 package edu.millersville.cs.bitsplease.model;
 
-public abstract class UMLSymbol {
+import javafx.event.EventDispatcher;
+import javafx.event.Event;
+import javafx.event.EventDispatchChain;
+
+public abstract class UMLSymbol implements EventDispatcher {
+	
+	public Event dispatchEvent(Event event, EventDispatchChain tail){
+		
+		return event;
+	}
 
 }
