9dc211d4ab0bbc6399460290bb651606564d6960
Merv Fansler <mmfansler@gmail.com>
2015-02-26 10:09:26 -0500

    demo dragging class object

--- src/edu/millersville/cs/bitsplease/view/DocumentViewPane.java
+++ src/edu/millersville/cs/bitsplease/view/DocumentViewPane.java
@@ -3,12 +3,38 @@
  */
 package edu.millersville.cs.bitsplease.view;
 
+import javafx.event.EventHandler;
+import javafx.geometry.Point2D;
+import javafx.scene.Group;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import edu.millersville.cs.bitsplease.model.UMLClassSymbol;
 
 /**
  * @author Mervin
  *
  */
 public class DocumentViewPane extends Pane {
+
+	/**
+	 * 
+	 */
+	public DocumentViewPane() {
+		super();
+		
+		UMLClassSymbol c = new UMLClassSymbol("First Class");
+		Group g = new Group();
+		UMLObjectView v = new UMLObjectView(c, g);
+		getChildren().add(g);
+		
+		g.addEventHandler(MouseEvent.MOUSE_DRAGGED, new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent e) {
+				//System.out.println(e);
+				v.UpdatePosition(new Point2D(e.getX(), e.getY()));
+			}
+		});
+	}
 
 }
