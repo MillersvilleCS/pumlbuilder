283decaff763a46b5f51d3d7a29aeb813fc5df30
jmmartel <jmmartel@millersville.edu>
2015-03-29 21:53:42 -0400

    added relation symbols for association, dependency, composition, aggregation, and generalization as well as functions to handle a textfield for relations

--- src/edu/millersville/cs/bitsplease/model/UMLRelationSymbol.java
+++ src/edu/millersville/cs/bitsplease/model/UMLRelationSymbol.java
@@ -1,6 +1,7 @@
 /**
  * @author Michael Sims
  * @author Merv Fansler
+ * @author Joe Martello
  * @since February 25, 2015
  * @version 0.1.1
  */
@@ -13,7 +14,13 @@ import java.io.ObjectOutput;
 
 import javafx.beans.property.Property;
 import javafx.collections.ObservableList;
+import javafx.geometry.Point2D;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Polyline;
+import javafx.scene.shape.Shape;
 
 public class UMLRelationSymbol extends UMLSymbol {
 
@@ -21,6 +28,8 @@ public class UMLRelationSymbol extends UMLSymbol {
 	private UMLObjectSymbol sourceObject;
 	private UMLObjectSymbol targetObject;
 	private Line rLine;
+	private Shape rSymbol;
+	private TextField rText;
 	
 	public UMLRelationSymbol(UMLObjectSymbol sourceObject,
 			UMLObjectSymbol targetObject, UMLRelationType relationType){
@@ -32,21 +41,56 @@ public class UMLRelationSymbol extends UMLSymbol {
 		
 		// clicks should NOT be captured based on bounding box
 		this.setPickOnBounds(false);
-		
 		this.rLine = new Line();
+		refreshLine();
+		getChildren().add(rLine);
+		
+		rText = new TextField();
+		rText.setMouseTransparent(true);
+		rText.setFocusTraversable(false);
+		rText.setVisible(false);
+		rText.setStyle("-fx-border-color: white");
+		rText.setPrefWidth(100);
+		getChildren().add(rText);
 		
 		switch (this.relationType) {
 		case ASSOCIATION:
+			rSymbol = new Polyline();
+			refreshArrow();
 			break;
 		case DEPENDENCY:
+			rSymbol = new Polyline();
+			refreshArrow();
 			rLine.getStrokeDashArray().addAll(25d, 10d);
+			break;
+		case AGGREGATION:
+			rSymbol = new Polygon(5.0, 0.0,
+					0.0 ,8.0,
+					5.0, 16.0,
+					10.0, 8.0);
+			rSymbol.setFill(Color.WHITE);
+			rSymbol.setStroke(Color.BLACK);
+			refreshDiamond();
+			break;
+		case COMPOSITION:
+			rSymbol = new Polygon(5.0, 0.0,
+					0.0 ,8.0,
+					5.0, 16.0,
+					10.0, 8.0);
+			refreshDiamond();
+			break;
+		case GENERALIZATION:
+			rSymbol = new Polygon(12.0, 0.0,
+					0.0 ,15.0,
+					24.0, 15.0);
+			rSymbol.setFill(Color.WHITE);
+			rSymbol.setStroke(Color.BLACK);
+			refreshTriangle();
 			break;
 		default:
 			break;
 		}
-		
-		refreshLine();
-		getChildren().add(rLine);
+		getChildren().add(rSymbol);
 	}
 	
 	/**
@@ -97,6 +141,7 @@ public class UMLRelationSymbol extends UMLSymbol {
 			rLine.setEndY(targetObject.getTopCenter().getY());
 			break;			
 		}
+		
 	}
 	
 	public UMLRelationType getUmlRelationType() {
@@ -123,8 +168,140 @@ public class UMLRelationSymbol extends UMLSymbol {
 		this.targetObject = _target;
 	}
 
+	/**
+	 * resets the position of the arrowhead based off of the line's position
+	 */
+	private void refreshArrow(){
+		int length = 10;
+		Polyline p = (Polyline) rSymbol;
+		Point2D lpoint;
+		Point2D rpoint;
+		double x = rLine.getEndX();
+		double y = rLine.getEndY();
+		
+		if ((x == targetObject.getTopCenter().getX()) && (y == targetObject.getTopCenter().getY())){
+			lpoint = new Point2D(x - length, y - length);
+			rpoint = new Point2D(x + length, y - length);
+		} else if ((x == targetObject.getBottomCenter().getX()) && (y == targetObject.getBottomCenter().getY())){
+			lpoint = new Point2D(x - length, y + length);
+			rpoint = new Point2D(x + length, y + length);
+		} else if ((x == targetObject.getMiddleLeft().getX()) && (y == targetObject.getMiddleLeft().getY())){
+			lpoint = new Point2D(x - length, y - length);
+			rpoint = new Point2D(x - length, y + length);
+		} else {
+			lpoint = new Point2D(x + length, y - length);
+			rpoint = new Point2D(x + length, y + length);
+		}
+		
+		p.getPoints().setAll(lpoint.getX(), lpoint.getY(),
+								x, y,
+								rpoint.getX(), rpoint.getY());
+	}
+	/**
+	 * resets the position for the AGGREGATION and COMPOSITION relation types
+	 */
+	private void refreshDiamond(){
+		Polygon p = (Polygon) rSymbol;
+		double x = rLine.getEndX();
+		double y = rLine.getEndY();
+		p.setRotate(0);
+		rSymbol.setLayoutX(x - 5);
+		
+		if ((x == targetObject.getTopCenter().getX()) && (y == targetObject.getTopCenter().getY())){
+			rSymbol.setLayoutY(y);
+			rSymbol.setLayoutY(y - 16);
+		} else if ((x == targetObject.getBottomCenter().getX()) && (y == targetObject.getBottomCenter().getY())){
+			rSymbol.setLayoutY(y);
+		} else if ((x == targetObject.getMiddleLeft().getX()) && (y == targetObject.getMiddleLeft().getY())){
+			p.setRotate(90);
+			rSymbol.setLayoutX(x - 12);
+			rSymbol.setLayoutY(y - 8);
+		} else {
+			p.setRotate(90);
+			rSymbol.setLayoutX(x + 3);
+			rSymbol.setLayoutY(y - 8);
+		}
+	}
+	
+	/**
+	 * resets the position of the GENERALIZATION relation type
+	 */
+	private void refreshTriangle(){
+		Polygon p = (Polygon) rSymbol;
+		double x = rLine.getEndX();
+		double y = rLine.getEndY();
+		p.setRotate(0);
+		rSymbol.setLayoutX(x - 12);
+		
+		if ((x == targetObject.getTopCenter().getX()) && (y == targetObject.getTopCenter().getY())){
+			rSymbol.setLayoutY(y);
+			rSymbol.setRotate(180);
+			rSymbol.setLayoutY(y - 16);
+		} else if ((x == targetObject.getBottomCenter().getX()) && (y == targetObject.getBottomCenter().getY())){
+			rSymbol.setLayoutY(y);
+		} else if ((x == targetObject.getMiddleLeft().getX()) && (y == targetObject.getMiddleLeft().getY())){
+			rSymbol.setLayoutY(y - 8);
+			p.setRotate(90);
+			rSymbol.setLayoutX(x - 18);
+		} else {
+			p.setRotate(-90);
+			rSymbol.setLayoutX(x - 5);
+			rSymbol.setLayoutY(y - 8);
+		}
+	}
+	
+	/**
+	 * makes the textField visible and editable to the user
+	 */
+	public void editText(){
+		rText.setMouseTransparent(false);
+		rText.setVisible(true);
+	}
+	
+	/**
+	 * prevents the textField from being edited
+	 */
+	public void stopEdit(){
+		rText.setMouseTransparent(true);
+	}
+	
+	/**
+	 * resets the contents of the textField and makes it invisible to the user
+	 */
+	public void deleteText(){
+		rText.setText("");
+		rText.setMouseTransparent(true);
+		rText.setVisible(false);
+	}
+	
+	/**
+	 * calls all of the related functions to update the relation line and symbols
+	 */
 	public void refresh() {
 		refreshLine();
+		
+		switch (this.relationType) {
+		case ASSOCIATION:
+			refreshArrow();
+			break;
+		case DEPENDENCY:
+			refreshArrow();
+			break;
+		case AGGREGATION:
+			refreshDiamond();
+			break;
+		case COMPOSITION:
+			refreshDiamond();
+			break;
+		case GENERALIZATION:
+			refreshTriangle();
+			break;
+		default:
+			break;
+		}
+		
+		rText.setLayoutX((rLine.getStartX() + rLine.getEndX()) / 2);
+		rText.setLayoutY((rLine.getStartY() + rLine.getEndY()) / 2);
 	}
 
 	@Override
