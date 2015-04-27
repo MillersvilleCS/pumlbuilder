d5c7cd45c7a7bf0e27288ee35fd348ab85774ddf
msimsjr <mike.sims.jr@gmail.com>
2015-04-27 17:52:04 -0400

    junit UseCase tests

---
+++ test/UseCaseInitTest.java
@@ -0,0 +1,58 @@
+
+/**
+ * @author Mike Sims
+ * @version 0.3.0
+ */
+
+import static org.junit.Assert.*;
+import javafx.geometry.Point2D;
+import pumlbuilder.JavaFXThreadingRule;
+import edu.millersville.cs.bitsplease.model.UMLUseCaseSymbol;
+
+import org.junit.*;
+
+public class UseCaseInitTest {
+
+	@Rule
+	public JavaFXThreadingRule javafxrule = new JavaFXThreadingRule();
+	
+	UMLUseCaseSymbol useCaseInit, useCaseLocation, useCaseText;
+	
+	//Initialization Test
+	@Before
+	public void UseCaseInitializing() {
+		useCaseInit = new UMLUseCaseSymbol(new Point2D(100,100));
+	}
+	
+	@Test
+	public void UseCaseInitialzingTest() {
+		assertTrue(100 == useCaseInit.getX());
+		assertTrue(100 == useCaseInit.getY());
+		assertEquals("Untitled Use Case", useCaseInit.getIdentifier());
+	}
+	
+	//Editing Location Test
+	@Before
+	public void UseCaseEditingLocation() {
+		useCaseLocation = new UMLUseCaseSymbol(new Point2D(100,100));
+		useCaseLocation.setOrigin(new Point2D(200,200));
+	}
+	
+	@Test
+	public void UseCaseLocationTest() {
+		assertTrue(200 == useCaseLocation.getX());
+		assertTrue(200 == useCaseLocation.getY());
+	}
+	
+	//Editing Text Test
+	@Before
+	public void UseCaseEditingText() {
+		useCaseText = new UMLUseCaseSymbol(new Point2D(100,100));
+		useCaseText.setIdentifier("Changed Use Case Text");
+	}
+	
+	@Test
+	public void UseCaseTextTest() {
+		assertEquals("Changed Use Case Text", useCaseText.getIdentifier());
+	}
+}
