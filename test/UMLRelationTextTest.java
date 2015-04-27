d963834644ffcc1ea4d2e2c9b4c8611203a83485
msimsjr <mike.sims.jr@gmail.com>
2015-04-27 17:20:49 -0400

    added more test to UMLRelationTextTest.java

--- test/UMLRelationTextTest.java
+++ test/UMLRelationTextTest.java
@@ -6,9 +6,7 @@
 import static org.junit.Assert.*;
 import javafx.geometry.Point2D;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 
 import pumlbuilder.JavaFXThreadingRule;
 import edu.millersville.cs.bitsplease.model.UMLClassSymbol;
@@ -18,9 +16,15 @@ import edu.millersville.cs.bitsplease.model.UMLRelationType;
 
 public class UMLRelationTextTest {
 	@Rule
-	public JavaFXThreadingRule jfxRule = new JavaFXThreadingRule();
+	public JavaFXThreadingRule jfxRule = new 
+
+JavaFXThreadingRule();
 	
-	UMLRelationSymbol depen, assoc, aggre, compo, gener;
+	UMLRelationSymbol depen, depenEdit,
+					  assoc, assocEdit,
+					  aggre, aggreEdit,
+					  compo, compoEdit,
+					  gener, generEdit;
 	
 	//Test dependency relation text field
 	@Before
@@ -35,6 +39,22 @@ public class UMLRelationTextTest {
 		assertEquals("Untitled dependency", depen.getIdentifier());
 	}
 	
+	//Test editing dependency relation text field
+	@Before
+	public void editDependency() throws Exception {
+		UMLClassSymbol cEdit1 = new UMLClassSymbol(new Point2D(0,100), 100, 100);
+		UMLClassSymbol cEdit2 = new UMLClassSymbol(new Point2D(10,100), 100, 100);
+		depenEdit = new UMLRelationSymbol(cEdit1, cEdit2, UMLRelationType.DEPENDENCY);
+		depenEdit.setIdentifier("Changed dependency text");
+	}
+	
+	@Test
+	public void testEditedTextDependency() {
+		assertEquals("Changed dependency text", depenEdit.getIdentifier());
+	}
+	
+	
+	//-----------------------------------------------------------------------
 	//Test association relation text field
 	@Before
 	public void setUpAssociation() throws Exception {
@@ -48,6 +68,22 @@ public class UMLRelationTextTest {
 		assertEquals("Untitled association", assoc.getIdentifier());
 	}
 	
+	//Test editing dependency relation text field
+	@Before
+	public void editAssociation() throws Exception {
+		UMLClassSymbol cEdit3 = new UMLClassSymbol(new Point2D(0,100), 100, 100);
+		UMLClassSymbol cEdit4 = new UMLClassSymbol(new Point2D(10,100), 100, 100);
+		assocEdit = new UMLRelationSymbol(cEdit3, cEdit4, UMLRelationType.ASSOCIATION);
+		assocEdit.setIdentifier("Changed association text");
+	}
+	
+	@Test
+	public void testEditedTextAssociation() {
+		assertEquals("Changed association text", assocEdit.getIdentifier());
+	}
+	
+	
+	//-----------------------------------------------------------------------
 	//Test aggregation relation text field
 	@Before
 	public void setUpAggregation() throws Exception {
@@ -61,6 +97,22 @@ public class UMLRelationTextTest {
 		assertEquals("Untitled aggregation", aggre.getIdentifier());
 	}
 	
+	//Test editing aggregation relation text field
+	@Before
+	public void editAggregation() throws Exception {
+		UMLClassSymbol cEdit5 = new UMLClassSymbol(new Point2D(0,100), 100, 100);
+		UMLClassSymbol cEdit6 = new UMLClassSymbol(new Point2D(10,100), 100, 100);
+		aggreEdit = new UMLRelationSymbol(cEdit5, cEdit6, UMLRelationType.AGGREGATION);
+		aggreEdit.setIdentifier("Changed aggregation text");
+	}
+	
+	@Test
+	public void testEditedTextAggregation() {
+		assertEquals("Changed aggregation text", aggreEdit.getIdentifier());
+	}
+	
+	
+	//-----------------------------------------------------------------------
 	//Test composition relation text field
 	@Before
 	public void setUpComposition() throws Exception {
@@ -74,6 +126,22 @@ public class UMLRelationTextTest {
 		assertEquals("Untitled composition", compo.getIdentifier());
 	}
 	
+	//Test editing aggregation relation text field
+	@Before
+	public void editComposition() throws Exception {
+		UMLClassSymbol cEdit7 = new UMLClassSymbol(new Point2D(0,100), 100, 100);
+		UMLClassSymbol cEdit8 = new UMLClassSymbol(new Point2D(10,100), 100, 100);
+		compoEdit = new UMLRelationSymbol(cEdit7, cEdit8, UMLRelationType.COMPOSITION);
+		compoEdit.setIdentifier("Changed composition text");
+	}
+	
+	@Test
+	public void testEditedTextComposition() {
+		assertEquals("Changed composition text", compoEdit.getIdentifier());
+	}
+	
+	
+	//-----------------------------------------------------------------------
 	//Test generalization relation text field
 	@Before
 	public void setUpGeneralization() throws Exception {
@@ -85,5 +153,19 @@ public class UMLRelationTextTest {
 	@Test
 	public void testTextFieldGeneralization() {
 		assertEquals("Untitled generalization", gener.getIdentifier());
+	}
+	
+	//Test editing generalization relation text field
+	@Before
+	public void editGeneralization() throws Exception {
+		UMLClassSymbol cEdit9 = new UMLClassSymbol(new Point2D(0,100), 100, 100);
+		UMLClassSymbol cEdit10 = new UMLClassSymbol(new Point2D(10,100), 100, 100);
+		generEdit = new UMLRelationSymbol(cEdit9, cEdit10, UMLRelationType.GENERALIZATION);
+		generEdit.setIdentifier("Changed generalization text");
+	}
+	
+	@Test
+	public void testEditedTextGeneralization() {
+		assertEquals("Changed generalization text", generEdit.getIdentifier());
 	}
 }
